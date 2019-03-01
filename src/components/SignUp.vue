57654d4de875164983cb6232be73e2f54e7b0f69
Makc <tvinkiman@gmail.com>
2019-03-01 15:52:19 +0200

    add style for signUp page

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="sign-up">
-        <p>Let's create a new account !</p>
-        <input type="text" v-model="email" placeholder="Email"><br />
-        <input type="password" v-model="password" placeholder="Password"><br />
-        <button v-on:click="signUp">Sign Up</button>
+        <h3>Let's create a new account !</h3>
+        <form action="">
+          <input class="text-field" type="text" v-model="email" placeholder="Email"><br />
+          <input class="text-field" type="password" v-model="password" placeholder="Password"><br />
+          <button class="save-button" v-on:click="signUp">Sign Up</button>
+        </form>
         <span>or back to <router-link to="/login">login</router-link></span>
     </div>
 </template>
@@ -34,7 +36,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .signUp {
   margin-top: 40px;
 }
@@ -45,10 +47,71 @@ input {
   padding: 15px;
 }
 
+h3 {
+  font-size: 24px;
+}
+
+form {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  width: 60%;
+  margin: 0 auto;
+}
+
 button {
-  margin-top: 10px;
-  widows: 10%;
+  // margin: 20px auto 0;
+  width: 45%;
+  padding: 7px 0;
+  border: 1px solid black;
+  background: none;
+  border-radius: 25px;
+  color: #000000;
+  font-weight: bold;
+  outline: none;
   cursor: pointer;
+  transition: all ease .4s;
+  font-size: 18px;
+  &:hover {
+    background: black;
+    color: white;
+  }
+}
+
+  .text-field {
+    width: 40%;
+    padding: 15px 10px;
+    margin: 0 0 15px;
+    border: none;
+    border-bottom: 1px solid black;
+    background: lightgray;
+    outline: none;
+    transition: ease all .4s;
+    font-size: 18px;
+
+    &:focus {
+      background: gray;
+      color: #ffffff;
+    }
+  }
+
+.save-button {
+  display: flex;
+  justify-content: center;
+  width: 30%;
+  padding: 7px 0;
+  border: 1px solid black;
+  background: none;
+  border-radius: 25px;
+  cursor: pointer;
+  outline: none;
+  font-size: 24px;
+  transition: all ease .4s;
+  
+  &:hover {
+    background: black;
+    color: white;
+  }
 }
 
 span {
